507e4a90fbc4e967cbb1a182addde37714602683
MalditoBarbudo <victorgrandagarcia@gmail.com>
2023-07-12 13:23:56 +0200

    test-benchmark with prototype of vignette

--- tests/testthat/test-benchmark.R
+++ tests/testthat/test-benchmark.R
@@ -116,15 +116,60 @@ filter_list <- show_plots_from(
   dplyr::filter(INVYR %in% c(2005, 2015)) |>
   dplyr::group_by(STATECD, COUNTYCD, PLOT) |>
   dplyr::filter(length(INVYR) > 1) |>
-  dplyr::group_by(INVYR, STATECD, COUNTYCD) |>
-  dplyr::slice_sample(prop = 0.1) |>
-  dplyr::group_by(STATECD) |>
-  dplyr::group_split() |>
-  purrr::set_names("CA", "OR", "WA") |>
-  purrr::imap(
-    .f = \(state_data, state_name) {esus:::.transform_plot_summary(state_data, c(2005, 2015), state_name)}
-  ) |>
-  purrr::flatten()
+  dplyr::group_by(INVYR, STATECD) |>
+  dplyr::slice_sample(prop = 0.2) |>
+  dplyr::group_by(STATECD, COUNTYCD, PLOT) |>
+  dplyr::filter(length(INVYR) > 1) |>
+  create_filter_list_fia()
+
+future::plan(future.callr::callr, workers = 6)
+res <- fia_to_tibble(
+  years = c(2005, 2015),
+  states = c("OR", "CA", "WA"),
+  filter_list = filter_list,
+  folder = "../international_inventories/data/fia/FIA_DATAMART_MARCH_2023/"
+) |>
+  dplyr::filter(purrr::map_lgl(tree, \(x) {!nrow(x) < 1})) |>
+  dplyr::group_by(ID_UNIQUE_PLOT) |>
+  dplyr::filter(length(YEAR) > 1) |>
+  sf::st_as_sf(coords = c("LON", "LAT"), crs = sf::st_crs(4326))
+
+test <- res |>
+  tidyr::unnest(tree, names_sep = "_") |>
+  dplyr::group_by(YEAR, ID_UNIQUE_PLOT) |>
+  dplyr::summarise(DIA = mean(tree_DIA, na.rm = TRUE), HT = mean(tree_HT, na.rm = TRUE))
+
+states_map <- maps::map("state", plot = FALSE, fill = TRUE) |>
+  sf::st_as_sf() |>
+  dplyr::filter(ID %in% c("california", "oregon", "washington"))
+
+par(mfrow = c(1,2))
+mapsf::mf_theme("darkula")
+mapsf::mf_shadow(states_map)
+mapsf::mf_map(
+  x = states_map, type = "base", add = TRUE
+)
+mapsf::mf_map(
+  x = test |> dplyr::filter(YEAR == 2005),
+  var = c("DIA", "HT"),
+  type = "prop_choro",
+  alpha = 0.5
+)
+mapsf::mf_title("2005")
+mapsf::mf_theme("darkula")
+mapsf::mf_shadow(states_map)
+mapsf::mf_map(
+  x = states_map, type = "base", add = TRUE
+)
+mapsf::mf_map(
+  x = test |> dplyr::filter(YEAR == 2015),
+  var = c("DIA", "HT"),
+  type = "prop_choro",
+  alpha = 0.5
+)
+mapsf::mf_title("2015")
+dev.off()
+
 
 # tictoc::tic()
 # future::plan(future.callr::callr, workers = 6)
