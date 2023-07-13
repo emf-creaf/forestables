9c528ee6b254db99190ae0a080aaacf17adeecc5
adrianatovar <odris2017@gmail.com>
2023-07-13 10:20:02 +0200

    adding ffi_growth_habit

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -1,10 +1,11 @@
 ## FIA states Dict
 fia_states_dictionary <- FIESTA::ref_statecd
 
-
+ffi_growth_habit <- iris
 
 # use internal data
 usethis::use_data(
   fia_states_dictionary,
+  ffi_growth_habit,
   overwrite = TRUE, internal = TRUE
 )
