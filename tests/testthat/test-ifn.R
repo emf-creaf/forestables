55ba313d5c37bde4e162bc99b7007906ad8085ec
adrianatovar <odris2017@gmail.com>
2023-11-23 16:02:50 +0100

    IFN: basic test tree table ifn3 y 4

--- tests/testthat/test-ifn.R
+++ tests/testthat/test-ifn.R
@@ -32,123 +32,131 @@ test_plots <- list(
 
 test_provinces <- names(test_plots)
 test_folder <- Sys.getenv("ifn_path")
- test_version = "ifn3"
-
-test_input <- .build_ifn_input_with (
-  test_version,
-  test_provinces,
-  test_plots, 
-  test_folder,
-  .verbose = TRUE, 
-)
+#  test_version = "ifn3"
+# 
+# test_input <- .build_ifn_input_with (
+#   test_version,
+#   test_provinces,
+#   test_plots, 
+#   test_folder,
+#   .verbose = TRUE, 
+# )
 
  test_especies <- ESPECIES
  test_provinces_dictionary <- ifn_provinces_dictionary
- 
-# 
-# test_that("ifn_tree_table_process works as intended", {
-#   
-#   test_version = "ifn2"
-#   test_input <- .build_ifn_input_with (
-#     test_version,
-#     test_provinces,
-#     test_plots, 
-#     test_folder,
-#     .verbose = TRUE, 
-#   )
-# 
-#   expected_names <- c(
-#     "ID_UNIQUE_PLOT",
-#     "province_code",
-#     "PLOT",
-#     "SP_CODE",
-#     "SP_NAME",
-#     "DIA",
-#     "HT",
-#     "DENSITY"
-#   )
-# 
-#   # object
-# 
-# 
-#     expect_s3_class(
-#     test_res <- ifn_tree_table_process(
-#       test_input$tree_table[3],
-#       test_version,
-#       test_input$plots[3],
-#       test_input$province[3],
-#       test_especies
-#     ),
-#     "tbl"
-#   )
-# 
-#   # data integrity
-#   expect_named(test_res, expected_names, ignore.order = TRUE)
-#   expect_true(nrow(test_res) > 0)
-# 
-# 
-#   expect_length(unique(test_res$PLOT), 1)
-#   expect_length(unique(test_res$province_code), 1)
-# 
-# 
-#   expect_identical(unique(test_res$PLOT)|> as.character(), test_input$plots[3] |> as.character())
-#   #CHECK THIS AGAIN, BOTH SHOULD BE CHARACTER
-#   expect_identical(unique(test_res$province_code) |> as.numeric(), test_input$province[3]|> as.numeric())
-# 
-#   # errors
-#   expect_warning(
-#     test_error <- ifn_tree_table_process(
-#       NA_character_,
-#       test_version,
-#       test_input$plots[3],
-#       test_input$province[3],
-#       test_especies
-#     ),
-#     "Some files"
-#   )
-#   expect_s3_class(test_error, "tbl")
-#   expect_true(nrow(test_error) < 1)
-# 
-#    # error in department name, gives an empty tibble
-#    expect_s3_class(
-#      test_error <- suppressWarnings(ifn_tree_table_process(
-#        test_input$tree_table[66],
-#        test_version,
-#        test_input$plots[66],
-#        test_input$province[66],
-#        test_especies
-#      )),
-#      "tbl"
-#    )
-#    expect_true(nrow(test_error) < 1)
-#    # error in plot name, should return an empty tibble
-#    expect_s3_class(
-#      test_error <- suppressWarnings(ifn_tree_table_process(
-#        test_input$tree_table[64],
-#        test_version,
-#        test_input$plots[64],
-#        test_input$province[64],
-#        test_especies
-#      )),
-#      "tbl"
-#    )
-#    expect_true(nrow(test_error) < 1)
-# })
 
 
 test_that("ifn_tree_table_process works as intended", {
+
+  test_version = "ifn2"
+  test_input <- .build_ifn_input_with (
+    test_version,
+    test_provinces,
+    test_plots,
+    test_folder,
+    .verbose = TRUE,
+  )
+
+  expected_names <- c(
+    "ID_UNIQUE_PLOT",
+    "province_code",
+    "PLOT",
+    "SP_CODE",
+    "SP_NAME",
+    "DIA",
+    "HT",
+    "DENSITY"
+  )
+
+  # object
+
+
+    expect_s3_class(
+    test_res <- ifn_tree_table_process(
+      test_input$tree_table[3],
+      test_version,
+      test_input$plots[3],
+      test_input$province[3],
+      test_especies
+    ),
+    "tbl"
+  )
+
+  # data integrity
+  expect_named(test_res, expected_names, ignore.order = TRUE)
+  expect_true(nrow(test_res) > 0)
+
+
+  expect_length(unique(test_res$PLOT), 1)
+  expect_length(unique(test_res$province_code), 1)
+
+
+  expect_identical(unique(test_res$PLOT)|> as.character(), test_input$plots[3] |> as.character())
+  #CHECK THIS AGAIN, BOTH SHOULD BE CHARACTER
+  expect_identical(unique(test_res$province_code) |> as.numeric(), test_input$province[3]|> as.numeric())
+
+  # errors
+  expect_warning(
+    test_error <- ifn_tree_table_process(
+      NA_character_,
+      test_version,
+      test_input$plots[3],
+      test_input$province[3],
+      test_especies
+    ),
+    "Some files"
+  )
+  expect_s3_class(test_error, "tbl")
+  expect_true(nrow(test_error) < 1)
+
+   # error in department name, gives an empty tibble
+   expect_s3_class(
+     test_error <- suppressWarnings(ifn_tree_table_process(
+       test_input$tree_table[66],
+       test_version,
+       test_input$plots[66],
+       test_input$province[66],
+       test_especies
+     )),
+     "tbl"
+   )
+   expect_true(nrow(test_error) < 1)
+   # error in plot name, should return an empty tibble
+   expect_s3_class(
+     test_error <- suppressWarnings(ifn_tree_table_process(
+       test_input$tree_table[64],
+       test_version,
+       test_input$plots[64],
+       test_input$province[64],
+       test_especies
+     )),
+     "tbl"
+   )
+   expect_true(nrow(test_error) < 1)
+})
+
+
+test_that("ifn_tree_table_process for ifn3 works as intended", {
   
-  version_test<- "ifn3"
+  test_version<- "ifn3"
+  test_input <- .build_ifn_input_with (
+    test_version,
+    test_provinces,
+    test_plots,
+    test_folder,
+    .verbose = TRUE,
+  )
   
   expected_names <- c(
     "ID_UNIQUE_PLOT", 
+    "province_code",
     "Clase",
     "Subclase",
     "PLOT",
     "SP_CODE",
     "SP_NAME",
     #tree number id in ifn4
-    # "nArbol",
+     "nArbol",
     #CUALIDAD 6 = dead but providing functions
     "Calidad",
     "Forma",
@@ -160,6 +168,7 @@ test_that("ifn_tree_table_process works as intended", {
     #height in m
     "HT", 
     "DENSITY"
+    
   )
   
   
@@ -232,9 +241,43 @@ test_that("ifn_tree_table_process works as intended", {
   
 })
 
-test_that("ifn_tree_table_process works as intended", {
+test_that("ifn_tree_table_process for ifn4 works as intended", {
   
-  version_test<- "ifn4"
+  test_plots <- list(
+    # "01" = c(19,80,1120),
+    # # "02"= c(11,444,1839),
+    # "03"= c(626,1021,23),
+    # "04"= c(233,5,445),
+    # "05" = c(61, 14, 328),
+    "06" = c(2064,1138,325),
+    "07" = c(679,114,499),
+    "10" = c(3374,261)
+    # "12" = c(156,1463,377),
+    # "13" = c(51, 419,783),
+    # "17" = c(2003,629,2944),
+    # "23" = c(269,1460,444),
+    # "26" = c(960,495,172),
+    # "27" = c(90, 190,537),
+    # "30" = c(78,1223,1057),
+    # "33" = c(818,283,1483),
+    # "31" = c(135,761,1518),
+    # "38" = c(672,426,1557),
+    # "40" = c(412,1216,1728),
+    # # "50" = c(172, 479,744),
+    # "49" = c(105,99,532),
+     # "91" = c(1406115, 0),
+     # "tururu" = 3555
+  )
+  
+  test_provinces <- names(test_plots)
+  test_version<- "ifn4"
+  test_input <- .build_ifn_input_with (
+    test_version,
+    test_provinces,
+    test_plots,
+    test_folder,
+    .verbose = TRUE,
+  )
   
   expected_names <- c(
     "ID_UNIQUE_PLOT", 
@@ -250,7 +293,6 @@ test_that("ifn_tree_table_process works as intended", {
     "Calidad",
     "Forma",
     #check codes to understand origin and trace of individuals
-    "OrdenIf2",
     "OrdenIf3",
     "OrdenIf4",
     #diameter in cm
@@ -266,10 +308,10 @@ test_that("ifn_tree_table_process works as intended", {
   
   expect_s3_class(
     test_res <- ifn_tree_table_process(
-      test_input$tree_table[3],
+      test_input$tree_table[1],
       test_version,
-      test_input$plots[3],
-      test_input$province[3],
+      test_input$plots[1],
+      test_input$province[1],
       test_especies
     ),
     "tbl"
@@ -284,17 +326,17 @@ test_that("ifn_tree_table_process works as intended", {
   expect_length(unique(test_res$province_code), 1)
   
   
-  expect_identical(unique(test_res$PLOT)|> as.character(), test_input$plots[3] |> as.character())
+  expect_identical(unique(test_res$PLOT)|> as.character(), test_input$plots[1] |> as.character())
   #CHECK THIS AGAIN, BOTH SHOULD BE CHARACTER
-  expect_identical(unique(test_res$province_code) |> as.numeric(), test_input$province[3]|> as.numeric())
+  expect_identical(unique(test_res$province_code) |> as.numeric(), test_input$province[1]|> as.numeric())
   
   # errors
   expect_warning(
     test_error <- ifn_tree_table_process(
       NA_character_,
       test_version,
-      test_input$plots[3],
-      test_input$province[3],
+      test_input$plots[1],
+      test_input$province[1],
       test_especies
     ),
     "Some files"
@@ -305,10 +347,10 @@ test_that("ifn_tree_table_process works as intended", {
   # error in department name, gives an empty tibble
   expect_s3_class(
     test_error <- suppressWarnings(ifn_tree_table_process(
-      test_input$tree_table[66],
+      test_input$tree_table[9],
       test_version,
-      test_input$plots[66],
-      test_input$province[66],
+      test_input$plots[9],
+      test_input$province[9],
       test_especies
     )),
     "tbl"
@@ -317,10 +359,10 @@ test_that("ifn_tree_table_process works as intended", {
   # error in plot name, should return an empty tibble
   expect_s3_class(
     test_error <- suppressWarnings(ifn_tree_table_process(
-      test_input$tree_table[64],
+      test_input$tree_table[11],
       test_version,
-      test_input$plots[64],
-      test_input$province[64],
+      test_input$plots[11],
+      test_input$province[11],
       test_especies
     )),
     "tbl"
